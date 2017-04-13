7c593e33416abd5507fb2390ca979e5d0c138507
Lee <tmddn591@naver.com>
2017-04-13 14:47:06 +0900

    이대희는 5월 생 추가

--- Test.java
+++ Test.java
@@ -2,7 +2,8 @@ public class Test{
 	public static void main(String args[]){
 	
 	System.out.println("내생일");
-	System.out.println("입니다")
-	System.out.println("이대희 바보");
+	System.out.println("입니다");
+	System.out.println("이대희는 내 동생");
+	System.out.println("5월이니까 내 동생");
 	}
 }
