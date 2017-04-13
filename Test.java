0a782f894f0f9470dd3ceee0d088f9703ea82705
Lee <tmddn591@naver.com>
2017-04-13 14:46:17 +0900

    이대희 바보 추가

--- Test.java
+++ Test.java
@@ -2,6 +2,7 @@ public class Test{
 	public static void main(String args[]){
 	
 	System.out.println("내생일");
-	System.out.println("입니다");
+	System.out.println("입니다")
+	System.out.println("이대희 바보");
 	}
 }
